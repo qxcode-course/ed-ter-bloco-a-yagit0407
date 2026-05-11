fb592b896c3d4dfbfc545dc872304fc239f0909b
yagit0407 <yagogit0407@gmail.com>
2026-05-11 10:36:44 -0300

    b

--- ed/editor/src/go/draft.go
+++ ed/editor/src/go/draft.go
@@ -1,5 +1,127 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
+type Node struct {
+    value rune
+    prev *Node
+    next *Node
+}
+
+type Editor struct {
+    head *Node
+    tail *Node
+    cursor *Node
+}
+
+func NewEditor() *Editor {
+	head := &Node{}
+	tail := &Node{}
+
+	head.next = tail
+	tail.prev = head
+
+	return &Editor{
+		head:   head,
+		tail:   tail,
+		cursor: tail,
+	}
+}
+
+func (e *Editor) Insert (ch rune) {
+    node := &Node{value: ch}
+
+    prev := e.cursor.prev
+    
+    prev.next = node
+    node.prev = prev
+
+    node.next = e.cursor
+    e.cursor.prev = node
+
+}
+func (e *Editor) Left() {
+	if e.cursor.prev != e.head {
+		e.cursor = e.cursor.prev
+	}
+}
+
+func (e *Editor) Right() {
+	if e.cursor != e.tail {
+		e.cursor = e.cursor.next
+	}
+}
+
+func (e *Editor) Delete() {
+    if e.cursor == e.tail {
+        return
+    }
+    target := e.cursor
+
+    target.prev.next = target.next
+    target.next.prev = target.prev
+    
+    e.cursor = target.next
+}
+
+func (e *Editor) Backspace() {
+    if e.cursor.prev == e.head {
+        return
+    }
+    
+    target := e.cursor.prev
+    target.prev.next = e.cursor
+    e.cursor.prev = target.prev
+}
+
+func (e *Editor) String() string {
+    out := ""
+
+    for node := e.head.next; node != e.tail; node = node.next {
+        if node == e.cursor {
+            out += "|"
+        }
+        out += string(node.value)
+    } 
+    if e.cursor == e.tail {
+        out += "|"
+    }
+    return out
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	text := scanner.Text()
+
+	ed := NewEditor()
+
+	for _, ch := range text {
+
+		switch ch {
+
+		case '<':
+			ed.Left()
+
+		case '>':
+			ed.Right()
+
+		case 'B':
+			ed.Backspace()
+
+		case 'D':
+			ed.Delete()
+
+		case 'R':
+			ed.Insert('\n')
+
+		default:
+			ed.Insert(ch)
+		}
+	}
+
+	fmt.Println(ed.String())
 }
--- ed/prin_list1/src/go/main.go
+++ ed/prin_list1/src/go/main.go
@@ -7,30 +7,40 @@ import (
 
 // mostra a lista com o elemento sword destacado
 func ToStr(l *DList[int], sword *DNode[int]) string {
-	return ""
+	out := []string{}
+	for node := l.Front(); node != l.End(); node = node.Next() {
+		if node == sword {
+			out = append(out, fmt.Sprintf("[%d]", node.Value))
+		} else {
+			out = append(out, fmt.Sprintf("%d", node.Value))
+		}
+	}
+	return "[ " + strings.Join(out, " ") + " ]"
 }
 
 // move para frente na lista circular
 func Next(l *DList[int], it *DNode[int]) *DNode[int] {
-	return nil
+	if it.Next() == l.End() {
+		return l.Front()
+	}
+	return it.Next()
 }
 
 func main() {
 	var qtd, chosen int
 	fmt.Scan(&qtd, &chosen)
-	fmt.Println(qtd, chosen)
 	l := NewDList[int]()
 	for i := 1; i <= qtd; i++ {
 		l.PushBack(i)
 	}
 	sword := l.Front()
-	for range chosen - 1 {
-		sword = Next(l, sword)
+	for i := 0; i < chosen-1; i++ {
+	sword = Next(l, sword)
 	}
-	for range qtd - 1 {
-		fmt.Println(ToStr(l, sword))
-		l.Erase(Next(l, sword))
-		sword = Next(l, sword)
+	for i := 0; i < qtd-1; i++ {
+	fmt.Println(ToStr(l, sword))
+	l.Erase(Next(l, sword))
+	sword = Next(l, sword)
 	}
 	fmt.Println(ToStr(l, sword))
-}
+	}
